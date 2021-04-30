67d347dd03332a7f4e278a75dae43a1bc834ec83
plexinio <45472578+plexinio@users.noreply.github.com>
2021-04-30 21:22:37 +0200

    began to ban players

--- src/components/game/PlayerList.js
+++ src/components/game/PlayerList.js
@@ -2,20 +2,73 @@ import React from "react";
 import styled from "styled-components";
 import Message from "../../views/Message";
 import {ConservativeBox, FlexBox, VerticalScroller} from "../../views/design/Containers";
-import {DiscreetButton, LinkButton} from "../../views/design/Interaction";
+import {Button, DiscreetButton, LinkButton} from "../../views/design/Interaction";
 import {Spinner} from "../../views/design/Spinner";
 import {withRouter} from "react-router-dom";
 import User from "../shared/models/User";
 import {Info, Label} from "../../views/design/Text";
+import {api, handleError} from "../../helpers/api";
 
 /**
  * TODO there's probably more to add here (friend requests maybe)
  */
+
+const ButtonBan = styled.button`
+ &:hover {
+    transform: translateY(-2px);
+  }
+  padding: 3px;
+  font-weight: 700;
+  margin-left: 5px;
+  font-size: 15px;
+  font-family: Roboto;
+  text-align: center;
+  color: rgba(255, 255, 255, 1);
+  width: ${props => props.width || null};
+  height:  auto;
+  border: none;
+  border-radius: 2px;
+  
+  cursor: ${props => (props.disabled ? "default" : "pointer")};
+  opacity: ${props => (props.disabled ? 0.4 : 1)};
+  background: rgb(191,62,255);
+  transition: all 0.3s ease;
+  
+`;
 class PlayerList extends React.Component {
     constructor(params) {
         super(params);
     }
 
+    isGameMaster() {
+        return User.getAttribute('userId') == this.props.game['gameMaster'];
+    }
+
+    async banPlayer(game, nameofbannedplayer){
+        try {
+            // request setup
+            const url = `/games/${game.gameId}/ban`;
+            const requestBody = nameofbannedplayer;
+            const config = {headers: User.getUserAuthentication()};
+
+            // send request
+            const response = await api.put(url, requestBody, config);
+            console.log(response);
+
+
+            // const url2 =  `/games/${game.gameId}`;
+            // const response2 = await api.get(url2, config);
+            // console.log(response2)
+            // const players = await api.get(`/users`, { headers:{ userIds: response2.data.players } });
+            // console.log(players.data);
+
+
+        } catch (error) {
+            alert(`Something went wrong while banning the player: \n${handleError(error)}`);
+        }
+
+
+    }
 
     render() {
         if (!this.props.game || !this.props.players) {
@@ -38,10 +91,32 @@ class PlayerList extends React.Component {
                             <tr>
                                 <td><Label>{user.username}</Label></td>
                                 {game.gameState == 'LOBBY'? (
-                                    <td><Info>{game.playerStates[user.userId]}</Info></td>
+                                    <td>
+                                        <Info>{game.playerStates[user.userId]}
+
+                                        </Info>
+
+
+                                    </td>
                                 ) : (
                                     <td><Info>{game.scores[user.userId]}</Info></td>
                                 )}
+
+                            <td>
+
+                                {this.isGameMaster() && (user.userId != User.getAttribute('userId'))?(
+
+                                    <ButtonBan onClick={() => {
+                                        this.banPlayer(game, user.username);
+                                    }}>
+                                        Ban
+                                    </ButtonBan>
+
+
+
+                                ): null}
+
+                            </td>
                             </tr>
                         );
                     })}
--- src/components/lobby/GameSummary.js
+++ src/components/lobby/GameSummary.js
@@ -8,8 +8,31 @@ import Form from "../general/Form";
 import {Label} from "../../views/design/Text";
 import {Spinner} from "../../views/design/Spinner";
 import {BaseContainer} from "../../helpers/layout";
+import {Button} from "../../views/design/Interaction";
+import styled from "styled-components";
 
-
+const ButtonLogin = styled.button`
+ &:hover {
+    transform: translateY(-2px);
+  }
+  padding: 10px;
+  font-weight: 700;
+  
+  font-size: 15px;
+  font-family: Roboto;
+  text-align: center;
+  color: rgba(255, 255, 255, 1);
+  width: ${props => props.width || null};
+  height: auto;
+  border: none;
+  border-radius: 2px;
+  margin-right: 15px;
+  cursor: ${props => (props.disabled ? "default" : "pointer")};
+  opacity: ${props => (props.disabled ? 0.4 : 1)};
+  background: rgb(191,62,255);
+  transition: all 0.3s ease;
+  
+`;
 
 class GameSummary extends React.Component {
 
@@ -41,15 +64,29 @@ class GameSummary extends React.Component {
             {players.map(player => {
 
                 let score = game.scores[player.userId];
-                return <div style={{
-                    paddingBottom:'15px'
-                }}>
+                return <div >
                     <Label> {player.username}: {score}</Label>
                 </div>
             })}
         </BaseContainer>
     }
 
+    async gotoLobby() {
+        try {
+            // request setup
+            const url = `/games/${this.props.match.params['gameId']}/leave`;
+            const requestBody = "";
+            const config = {headers: User.getUserAuthentication()};
+
+            // send request
+            const response = await api.put(url, requestBody, config);
+            console.log(response);
+            this.props.history.push('/')
+
+        } catch (error) {
+            alert(`Something went wrong while leaving the game: \n${handleError(error)}`);
+        }
+    }
 
     render() {
         if (!this.props.game || !this.props.players) {
@@ -68,6 +105,16 @@ class GameSummary extends React.Component {
                 }}>
                     {this.aftermathInteractive()}
                 </div>
+                <ButtonLogin
+                    role="button"
+
+                    width="50%"
+                    onClick={() => {
+                        this.gotoLobby();
+                    }}
+                >
+                    Back to Lobby
+                </ButtonLogin>
             </div>
         );
     }
