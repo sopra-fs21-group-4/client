c3b3b452dce2fd36cb9c3d3a69945b615e7fa216
Philipp Marock <44873758+Sahibabdul@users.noreply.github.com>
2021-05-17 11:15:34 +0200

    Revert "Revert "Merge branch 'master' of https://github.com/sopra-fs21-group-4/client""
    This reverts commit c5969846542746d639f05fc47e22b9d9ef04a5a8.

--- src/components/game/GameSummary.js
+++ src/components/game/GameSummary.js
@@ -2,12 +2,13 @@ import React from 'react';
 import {api, handleError} from '../../helpers/api';
 import {Spinner} from '../../views/design/Spinner';
 import {withRouter} from 'react-router-dom';
-import {BackgroundDivLighter, VerticalList, VerticalScroller} from "../../views/design/Containers";
-import {Title} from "../../views/design/Text";
+import {BackgroundDivLighter, FlexBox, VerticalList, VerticalScroller} from "../../views/design/Containers";
+import {Info, Label, Title} from "../../views/design/Text";
 import User from "../shared/models/User";
 import GameRoundSummary from "../game/GameRoundSummary";
 import styled from "styled-components";
 import {Button} from "../../views/design/Interaction";
+import {BaseContainer} from "../../helpers/layout";
 
 
 const ButtonLogin = styled.button`
@@ -36,19 +37,20 @@ const ButtonLogin = styled.button`
 `;
 
 class GameSummary extends React.Component {
-    constructor(props) {
-        super(props);
+    constructor(params) {
+        super(params);
         this.state = {
-            game: null
+            game: null,
+
         };
     }
 
     async componentDidMount() {
         try {
             // request setup
-            const url = `/archive/games/${this.props.match.params.gameId}`;
             const config = {headers: User.getUserAuthentication()};
 
+            const url = `/archive/games/${this.props.match.params.gameId}`;
             const gameResponse = await api.get(url, config);
             console.log(gameResponse);
             this.setState({
@@ -58,6 +60,8 @@ class GameSummary extends React.Component {
             alert(`Something went wrong while fetching game info: \n${handleError(error)}`);
         }
     }
+
+
 
     async gotoLobby() {
         try {
@@ -76,16 +80,28 @@ class GameSummary extends React.Component {
         }
     }
 
+    ranking(i){
+        i = i+1;
+        return i;
+    }
+
     render() {
-        if (!this.state.game) {
+        if (!this.props.game || !this.props.players) {
             return <Spinner/>
         }
+        let i = 0;
+        let game = this.props.game;
+        let players = this.props.players.slice();
+        players.sort((a,b) => {return game.scores[b.userId] - game.scores[a.userId]});
+
         return <VerticalList
             style={{}}>
+
             <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
                 <div style={{display: 'flex', justifyContent: 'center',flexGrow: '1',}}>
-                    <Title>{this.state.game.name}</Title>
+                    <Title>{game.name}</Title>
                 </div>
+
                 <Button style={{
                     flexGrow: '1',
                     maxWidth: 'fit-content'
@@ -94,9 +110,31 @@ class GameSummary extends React.Component {
                             this.gotoLobby();
                         }}
                 >
-                    Back to Lobby1
+                    Back to Lobby
                 </Button>
             </div>
+            <BackgroundDivLighter style={{padding: '20px'}}>
+            <table style={{background: '#9ccfff'}} title={'loser'}>
+                <tr>
+                <td >
+                    <Info> Ranking:</Info> </td> <td>
+                    <Label>Player:</Label>
+                </td>
+                <td><Info>Score:</Info></td>
+                </tr>
+                {players.map((player, index) => {
+                    return (
+                        <tr>
+                        <td >
+                            <Info> {index+1}.</Info> </td> <td>
+                            <Label>   {player.username}</Label></td>
+                        <td><Info>{game.scores[player.userId]}</Info></td>
+                        </tr>
+                )})}
+
+
+            </table>
+            </BackgroundDivLighter>
             <BackgroundDivLighter>
                 {this.state.game.rounds.map(round => {
                     return <GameRoundSummary round={round}/>
